e69b1d705154ffe9d54b53073a9f94f9dfaefc98
HP123-1 <hinalpandya79@gmail.com>
2022-02-15 21:05:05 +0000

    backend project

---
+++ src/main/java/uk/co/bbc/model/sportsPojo.java
@@ -0,0 +1,13 @@
+package uk.co.bbc.model;
+
+public class sportsPojo {
+    private  String currency;
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
